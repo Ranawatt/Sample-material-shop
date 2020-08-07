4a93553062a7cecc4fed00d0d84de2dfa85afd81
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-08-07 18:41:23 +0530

    Created Helpers.kt class

---
+++ app/src/main/java/com/example/sample_material_shop/shop/Helpers.kt
@@ -0,0 +1,52 @@
+package com.example.sample_material_shop.shop
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
+import com.example.sample_material_shop.R
+
+val ProductColors = mapOf(
+    "Fiery Red"        to Color(0.634f, 0.000f, 0.000f),
+    "Deep Blue"        to Color(0.000f, 0.480f, 0.596f),
+    "Swirly Orange"    to Color(0.874f, 0.522f, 0.080f),
+    "Fantastic Yellow" to Color(0.859f, 0.808f, 0.020f)
+)
+
+val ProductColorProgression = mapOf(
+    "Fiery Red"        to "Deep Blue",
+    "Deep Blue"        to "Swirly Orange",
+    "Swirly Orange"    to "Fantastic Yellow",
+    "Fantastic Yellow" to "Fiery Red"
+)
+
+val ProductColorSampleImages = mapOf(
+    "Fiery Red"        to R.drawable.sample_red,
+    "Deep Blue"        to R.drawable.sample_blue,
+    "Swirly Orange"    to R.drawable.sample_orange,
+    "Fantastic Yellow" to R.drawable.sample_yellow
+)
+
+fun nextProductColor(color: String) = ProductColorProgression.getOrElse(color) { color }
+
+fun productColor(product: Product) =
+    ProductColors.getOrElse(product.color) { Color(1.0f, 1.0f, 1.0f) }
+
+val String.isProductColor: Boolean
+    get() = this != "N/A"
+
+@Composable
+fun SampleImage(color: String) {
+    Image(
+        modifier = Modifier.fillMaxWidth().height(180.dp),
+        contentScale = ContentScale.Crop,
+        asset = imageResource(
+            ProductColorSampleImages.getOrElse(color) { R.drawable.sample_orange }
+        )
+    )
+}
---
+++ app/src/main/java/com/example/sample_material_shop/shop/MainActivity.kt
@@ -0,0 +1,381 @@
+package com.example.sample_material_shop.shop
+
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import com.example.sample_material_shop.R
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.SurfaceView
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.compose.animation.animate
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.RowScope.gravity
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.sharp.Add
+import androidx.compose.material.icons.sharp.Remove
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModelProvider
+import com.example.sample_material_shop.R
+import com.google.android.filament.*
+import com.google.android.filament.Colors
+import com.google.android.filament.gltfio.AssetLoader
+import com.google.android.filament.gltfio.MaterialProvider
+import com.google.android.filament.gltfio.ResourceLoader
+import com.google.android.filament.utils.KtxLoader
+import com.google.android.filament.utils.Utils
+import kotlin.collections.set
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var storeViewModel: StoreViewModel
+    private lateinit var engine: Engine
+    private lateinit var assetLoader: AssetLoader
+    private lateinit var resourceLoader: ResourceLoader
+
+    private lateinit var indirectLight: IndirectLight
+    private lateinit var skybox: Skybox
+    private var light: Int = 0
+
+    companion object {
+        init { Utils.init() }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initFilament()
+
+        storeViewModel = ViewModelProvider(this).get(StoreViewModel::class.java)
+        val shoppingCart = storeViewModel.shoppingCart
+
+        val increase: (Product) -> Unit = { product ->
+            storeViewModel.update(product.copy(quantity = product.quantity + 1))
+        }
+
+        val decrease: (Product) -> Unit = { product ->
+            if (product.quantity > 1) {
+                storeViewModel.update(product.copy(quantity = product.quantity - 1))
+            } else {
+                storeViewModel.delete(product)
+            }
+        }
+
+        val updateColor: (Product) -> Unit = { product ->
+            storeViewModel.update(product.copy(color = nextProductColor(product.color)))
+        }
+
+        setContent {
+            StoreTheme {
+                Scaffold(
+                    topBar = { StoreAppBar() },
+                    floatingActionButton = { StoreCheckout(shoppingCart) }
+                ) { padding ->
+                    ShoppingCart(shoppingCart, increase, decrease, updateColor, padding)
+                }
+            }
+        }
+//        setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        engine.lightManager.destroy(light)
+        engine.destroyEntity(light)
+        engine.destroyIndirectLight(indirectLight)
+        engine.destroySkybox(skybox)
+
+        scenes.forEach {
+            engine.destroyScene(it.value.scene)
+            assetLoader.destroyAsset(it.value.asset)
+        }
+
+        assetLoader.destroy()
+        resourceLoader.destroy()
+
+        engine.destroy()
+    }
+
+    private fun initFilament() {
+        engine = Engine.create()
+        assetLoader = AssetLoader(engine, MaterialProvider(engine), EntityManager.get())
+        resourceLoader = ResourceLoader(engine)
+
+        val ibl = "courtyard_8k"
+        readCompressedAsset(this, "envs/${ibl}/${ibl}_ibl.ktx").let {
+            indirectLight = KtxLoader.createIndirectLight(engine, it)
+            indirectLight.intensity = 30_000.0f
+        }
+
+        readCompressedAsset(this, "envs/${ibl}/${ibl}_skybox.ktx").let {
+            skybox = KtxLoader.createSkybox(engine, it)
+        }
+
+        light = EntityManager.get().create()
+        val (r, g, b) = Colors.cct(6_000.0f)
+        LightManager.Builder(LightManager.Type.SUN)
+            .color(r, g, b)
+            .intensity(70_000.0f)
+            .direction(0.28f, -0.6f, -0.76f)
+            .build(engine, light)
+
+        fun createScene(name: String, gltf: String) {
+            val scene = engine.createScene()
+            val asset = readCompressedAsset(this, gltf).let {
+                val asset = loadModelGlb(assetLoader, resourceLoader, it)
+                transformToUnitCube(engine, asset)
+                asset
+            }
+            scene.indirectLight = indirectLight
+            scene.skybox = skybox
+
+            scene.addEntities(asset.entities)
+
+            scene.addEntity(light)
+
+            scenes[name] = ProductScene(engine, scene, asset)
+        }
+
+        createScene("Car paint", "models/car_paint/material_car_paint.glb")
+        createScene("Carbon fiber", "models/carbon_fiber/material_carbon_fiber.glb")
+        createScene("Lacquered wood", "models/lacquered_wood/material_lacquered_wood.glb")
+        createScene("Wood", "models/wood/material_wood.glb")
+    }
+
+}
+@Composable
+fun ShoppingCart(
+    shoppingCart: LiveData<List<Product>>,
+    increase: (Product) -> Unit,
+    decrease: (Product) -> Unit,
+    updateColor: (Product) -> Unit,
+    padding: InnerPadding
+) {
+    val products by shoppingCart.observeAsState(emptyList())
+    LazyColumnFor(items = products,
+        modifier = Modifier.padding(padding),
+        itemContent = { product ->
+            ShoppingCartItem(product, increase, decrease, updateColor) {
+                FilamentViewer(product)
+            }
+        }
+    )
+}
+
+@Composable
+fun ShoppingCartItem(
+    product: Product,
+    increase: (Product) -> Unit = { },
+    decrease: (Product) -> Unit = { },
+    updateColor: (Product) -> Unit = { },
+    content: @Composable () -> Unit = { }
+) {
+    val (selected, onSelected) = remember { mutableStateOf(false) }
+
+    val topLeftCornerRadius = animate(target = if (selected) 48.dp.value else 8.dp.value)
+    val cornerRadius        = animate(target = if (selected)  0.dp.value else 8.dp.value)
+
+    Surface(
+        modifier = Modifier.padding(16.dp, 8.dp, 16.dp, 8.dp),
+        shape = RoundedCornerShape(
+            topLeft = topLeftCornerRadius.dp,
+            topRight = cornerRadius.dp,
+            bottomLeft = cornerRadius.dp,
+            bottomRight = cornerRadius.dp
+        ),
+        elevation = 4.dp
+    ) {
+        Column {
+            Stack(
+                modifier = Modifier.toggleable(value = selected, onValueChange = onSelected)
+            ) {
+                content()
+
+                val selectedAlpha = animate(target = if (selected) 0.65f else 0.0f)
+                Surface(
+                    modifier = Modifier.matchParentSize(),
+                    color = MaterialTheme.colors.primary.copy(alpha = selectedAlpha)
+                ) {
+                    Icon(
+                        asset = Icons.Filled.Done,
+                        tint = contentColor().copy(alpha = selectedAlpha)
+                    )
+                }
+            }
+
+            ShoppingCartItemRow(product, decrease, increase, updateColor)
+        }
+    }
+}
+
+@Composable
+fun ShoppingCartItemRow(
+    product: Product,
+    decrease: (Product) -> Unit = { },
+    increase: (Product) -> Unit = { },
+    updateColor: (Product) -> Unit = { }
+) {
+    val hasColorSwatch = product.color.isProductColor
+    ConstraintLayout(modifier = Modifier.padding(12.dp).fillMaxWidth()) {
+        val (decreaseRef, increaseRef, labelRef, colorRef, amountRef) = createRefs()
+
+        SmallButton(
+            modifier = Modifier.constrainAs(decreaseRef) {
+                start.linkTo(parent.start)
+                centerVerticallyTo(parent)
+            },
+            onClick = { decrease(product) }
+        ) {
+            Image(Icons.Sharp.Remove)
+        }
+
+        SmallButton(
+            modifier = Modifier.constrainAs(increaseRef) {
+                start.linkTo(decreaseRef.end, margin = 4.dp)
+                centerVerticallyTo(parent)
+            },
+            onClick = { increase(product) }
+        ) {
+            Image(Icons.Sharp.Add)
+        }
+
+        Text(
+            modifier = Modifier.constrainAs(labelRef) {
+                start.linkTo(increaseRef.end, margin = 8.dp)
+                centerVerticallyTo(parent)
+            },
+            text = "${product.quantity}× ${product.material}"
+        )
+
+        if (hasColorSwatch) {
+            SmallButton(
+                modifier = Modifier.constrainAs(colorRef) {
+                    start.linkTo(labelRef.end, margin = 4.dp)
+                    centerVerticallyTo(parent)
+                },
+                onClick = { updateColor(product) },
+                color = Color.White
+            ) {
+                Box(
+                    modifier = Modifier.size(13.dp),
+                    shape = CircleShape,
+                    gravity = Alignment.Center,
+                    backgroundColor = productColor(product)
+                )
+            }
+        }
+
+        Text(
+            modifier = Modifier.constrainAs(amountRef) {
+                linkTo(
+                    start = (if (hasColorSwatch) colorRef else labelRef).end,
+                    end = parent.end,
+                    bias = 1.0f
+                )
+                centerVerticallyTo(parent)
+            },
+            text = formatAmount(product)
+        )
+    }
+}
+
+@Composable
+fun FilamentViewer(product: Product) {
+    var modelViewer by remember { mutableStateOf<ModelViewer?>(null) }
+
+    launchInComposition {
+        while (true) {
+            withFrameNanos { frameTimeNanos ->
+                modelViewer?.render(frameTimeNanos)
+            }
+        }
+    }
+
+    onCommit(product) {
+        val (engine, scene, asset) = scenes[product.material]!!
+        modelViewer?.scene = scene
+
+        asset.entities.find {
+            asset.getName(it)?.startsWith("car_paint_red") ?: false
+        }?.also { entity ->
+            val manager = engine.renderableManager
+            val instance = manager.getInstance(entity)
+            val material = manager.getMaterialInstanceAt(instance, 0)
+
+            val productColor = productColor(product)
+
+            val r = productColor.red
+            val g = productColor.green
+            val b = productColor.blue
+
+            material.setParameter(
+                "baseColorFactor", Colors.RgbaType.SRGB, r, g, b, 1.0f
+            )
+        }
+    }
+
+    AndroidView({ context ->
+        LayoutInflater.from(context).inflate(
+            R.layout.filament_host, FrameLayout(context), false
+        ).apply {
+            val (engine) = scenes[product.material]!!
+            modelViewer = ModelViewer(engine, this as SurfaceView).also {
+                setupModelViewer(it)
+            }
+        }
+    })
+}
+
+@Composable
+fun SmallButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { },
+    color: Color = MaterialTheme.colors.secondary,
+    content: @Composable () -> Unit = { }
+) {
+    Surface(
+        modifier = modifier.size(16.dp).gravity(Alignment.CenterVertically),
+        color = color,
+        shape = CircleShape,
+        elevation = 2.dp
+    ) {
+        Box(
+            modifier = Modifier.clickable(onClick = onClick),
+            gravity = Alignment.Center
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun StoreAppBar() {
+    TopAppBar(title = { Text(ContextAmbient.current.getString(R.string.app_name)) })
+}
+
+@Composable
+fun StoreCheckout(shoppingCart: LiveData<List<Product>>) {
+    val products = shoppingCart.observeAsState(emptyList()).value
+    ExtendedFloatingActionButton(
+        text = { Text("${products.sumBy { it.quantity }} items") },
+        icon = { Icon(Icons.Filled.ShoppingCart) },
+        onClick = { }
+    )
+}
